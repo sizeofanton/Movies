86b9d88f250d6da8a55dc23fae06470a4b0a209a
Anton Sizov <sizeofanton@gmail.com>
2020-11-23 19:13:10 +0300

    Homework1 task implementation

--- app/src/main/java/ru/mikhailskiy/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -5,23 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.tv_shows_fragment.*
 import ru.mikhailskiy.intensiv.R
+import ru.mikhailskiy.intensiv.data.MockRepository
+import ru.mikhailskiy.intensiv.data.Movie
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class TvShowsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,14 +32,39 @@ class TvShowsFragment : Fragment() {
         return inflater.inflate(R.layout.tv_shows_fragment, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TvShowsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tv_shows_recycler_view.layoutManager = LinearLayoutManager(context).apply {
+            orientation = LinearLayoutManager.VERTICAL
+        }
+        tv_shows_recycler_view.adapter = adapter.apply { addAll(listOf()) }
+
+        val tvShowsList =  MockRepository.getTvShows().map {
+            TvShowItem(it) { movie ->
+                openTvShowDetail(
+                    movie
+                )
             }
+        }
+
+        adapter.apply { addAll(tvShowsList) }
+
+
+    }
+
+    private fun openTvShowDetail(movie: Movie) {
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+
+        val bundle = Bundle()
+        bundle.putString("title", movie.title)
+        findNavController().navigate(R.id.movie_details_fragment, bundle, options)
     }
 }
